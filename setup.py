f8398b361e2f5eca910e036763ae49c9aa6ce8d0
latand <latand666@gmail.com>
2025-04-03 18:17:31 +0300

    chore: Bump version to 0.3.2 in setup.py to reflect recent updates.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name="chatgpt_md_converter",
-    version="0.3.1",
+    version="0.3.2",
     author="Kostiantyn Kriuchkov",
     author_email="latand666@gmail.com",
     description="A package for converting markdown to HTML for chat Telegram bots",
